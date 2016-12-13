dcc741d33329c80034556e67a2626ee35fb47bf9
dschmaryl <dschmaryl@gmail.com>
2016-12-13 00:57:40 -0500

    add static folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.static_folder = 'static'
+app.static_url_path = ''
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
