7db88042efb1b6ad05af37526e1b46750e9d876a
dschmaryl <dschmaryl@gmail.com>
2016-12-13 00:11:35 -0500

    fill in some login code

--- app/models.py
+++ app/models.py
@@ -3,8 +3,27 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
+    username = db.Column('username', db.String(32), unique=True, index=True)
+    password = db.Column('password', db.String(32))
+
+    # not sure if i want a name besides username
+    #name = db.Column('name', db.String(64), index=True)
+
     scores = db.relationship('Score', backref='golfer', lazy='dynamic')
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.name)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required
 
 from app import app, db, login_manager
+from .forms import LoginForm
 from .models import User
 
 
@@ -15,7 +17,39 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 
+@login_manager.user_loader
+def load_user(id):
+    return User.get(id)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(user, remember=True)
+        flash('logged in')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/new_score')
+@login_required
+def new_score():
+    # temporary
+    return redirect(url_for('index'))
