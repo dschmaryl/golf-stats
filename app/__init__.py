28a5e15674c6db46e6b65602fb2278f45a075c7a
dschmaryl <dschmaryl@gmail.com>
2016-12-12 22:35:47 -0500

    add flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from app import app, db
+from app import app, db, lm
 from .models import User
 
 
