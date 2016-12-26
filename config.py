ed668914154627fa15edb6e2726602d0e74735bf
dschmaryl <dschmaryl@gmail.com>
2016-12-26 11:17:32 -0500

    setup postgresql;

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+import os
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / 'stats.db')
-# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/golf_stats'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / 'stats.db')
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 SQLALCHEMY_MIGRATE_REPO = str(BASE_DIR / 'migrations')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
