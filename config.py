9de3781708bfa66127e3ab04b868c773896bc7d9
dschmaryl <dschmaryl@gmail.com>
2016-12-26 14:21:25 -0500

    move secret key to environ

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = str(BASE_DIR / 'migrations')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SECRET_KEY = 'double-secret-probation'
+SECRET_KEY = os.environ['SECRET_KEY']
