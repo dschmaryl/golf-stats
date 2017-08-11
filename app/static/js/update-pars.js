c13ec19889b5cc7990a97841fda652e79f2cf5d2
dschmaryl <dschmaryl@gmail.com>
2017-08-10 23:06:05 -0400

    return object with update method

--- app/static/js/update-pars.js
+++ app/static/js/update-pars.js
@@ -1,11 +1,13 @@
-var pars = function() {
+var loadPars = function() {
   var tees = getJson("tees-json");
 
-  return function() {
-    var selectedCourse = getSelected("course"),
-        selectedTeeColor = getSelected("tee-color");
-    tees[selectedCourse][selectedTeeColor].forEach( function(par, i) {
-      updateInner("hole" + (i + 1) + "_par", par);
-    })
+  return {
+    update: function() {
+      var selectedCourse = getSelected("course"),
+          selectedTeeColor = getSelected("tee-color");
+      tees[selectedCourse][selectedTeeColor].forEach( function(par, i) {
+        updateInner("hole" + (i + 1) + "_par", par);
+      })
+    }
   };
 };
