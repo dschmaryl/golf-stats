4b94428bfe99916119f2591b25a7af94d36703ba
dschmaryl <dschmaryl@gmail.com>
2017-08-10 20:18:15 -0400

    possibly use view to produce tees json

--- app/views/tees.py
+++ app/views/tees.py
@@ -1,3 +1,19 @@
-# put this somewhere better, not sure where
+# from flask import jsonify, request
+#
+# from app import app
+# from app.models import Round, Course, User
+
 
 TEES = ['white', 'red']
+
+
+# @app.route('/tees')
+# def tees():
+#     courses = Course.query.all()
+#     data = {}
+#     for course in courses:
+#         data[course.nickname] = {}
+#         for tee in course.tees:
+#             data[course.nickname][tee.color] = [tee.get_hole(i).par
+#                                                 for i in range(1, 19)]
+#     return jsonify(data)
