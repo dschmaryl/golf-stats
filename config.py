db1b436c49f9d2e0661053d8062828aa820db72b
dschmaryl <dschmaryl@gmail.com>
2017-09-20 13:40:07 -0400

    remove unnecessary config

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ import pathlib
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-SQLALCHEMY_MIGRATE_REPO = str(BASE_DIR / 'migrations')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 WTF_CSRF_ENABLED = True
