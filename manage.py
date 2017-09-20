ccce1ab28f7b21667aeceba364c372699e00b8b3
dschmaryl <dschmaryl@gmail.com>
2017-09-20 15:00:49 -0400

    insert lines

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -11,6 +12,7 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
     manager.run()
