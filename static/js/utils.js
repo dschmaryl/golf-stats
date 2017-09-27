c0ccb4b937f2ce6ef09d2fa23e20f885d005a628
dschmaryl <dschmaryl@gmail.com>
2017-09-26 21:15:00 -0400

    put js together

--- static/js/utils.js
+++ static/js/utils.js
@@ -10,19 +10,59 @@ var getValue = function(id) {
   return asNumber(parseInt(document.getElementById(id).value));
 };
 
-var getInner = function(id) {
-   return asNumber(parseInt(document.getElementById(id).innerHTML));
-};
-
 var getSelected = function(elementId) {
   var element = document.getElementById(elementId);
   return element.options[element.selectedIndex].text;
 };
 
-var getJson = function(elementId) {
-  return JSON.parse(document.getElementById(elementId).innerHTML);
-};
-
 var updateInner = function(id, value) {
   document.getElementById(id).innerHTML = value;
+};
+
+var createTotal = function(stat) {
+  var frontNineTotal = 0;
+  var backNineTotal = 0;
+
+  var nineHoleTotal = function(startHole, stat) {
+    return Array(9).fill().reduce((total, v, index) => {
+      var holeNumber = index + startHole;
+      return total + getValue('hole' + holeNumber + '_' + stat);
+    }, 0);
+  };
+
+  var updateTotals = function() {
+    updateInner('total-front-' + stat, frontNineTotal);
+    updateInner('total-back-' + stat, backNineTotal);
+    updateInner('total-' + stat, frontNineTotal + backNineTotal);
+  }
+
+  return {
+    updateFront: function() {
+      frontNineTotal = nineHoleTotal(1, stat);
+      updateTotals();
+    },
+    updateBack: function() {
+      backNineTotal = nineHoleTotal(10, stat);
+      updateTotals();
+    },
+    update: function() {
+      this.updateFront();
+      this.updateBack();
+    }
+  }
+};
+
+var loadPars = function() {
+  const tees = JSON.parse(document.getElementById("tees-json").innerHTML);
+
+  return {
+    update: function() {
+      var selectedCourse = getSelected("course");
+      var selectedTeeColor = getSelected("tee-color");
+
+      tees[selectedCourse][selectedTeeColor].forEach((par, i) => {
+        updateInner("hole" + (i + 1) + "_par", par);
+      })
+    }
+  };
 };
