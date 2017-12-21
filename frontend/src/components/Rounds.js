b0634789ee9e1b43c88f0ba278cb624075df5cd7
dschmaryl <dschmaryl@gmail.com>
2017-12-21 17:44:52 -0500

    reverse sort on reclick

--- frontend/src/components/Rounds.js
+++ frontend/src/components/Rounds.js
@@ -10,7 +10,6 @@ const cursorPointer = {cursor: 'pointer'};
 const headerLeft = {textAlign: 'left', fontWeight: 'bold'};
 const headerRight = {textAlign: 'right', fontWeight: 'bold'};
 
-
 function RoundsHeader(props) {
   function renderItem(value, key, className, reverse, style) {
     return (
@@ -92,34 +91,62 @@ function RoundsList(props) {
 export class Rounds extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {roundsData: props.roundsData};
+    this.state = {
+      roundsData: props.roundsData,
+      lastSortedBy: '',
+      lastSortReversed: false
+    };
   }
 
-  sortRows(sortBy, reversed) {
-    this.props.clickedSelected();
-    if (sortBy === 'date') {
-      this.setState({roundsData: this.props.roundsData})
-    } else {
-      this.setState({
-        roundsData: Object.values(this.props.roundsData).sort((a, b) => {
+  sortRounds(sortBy, reversed) {
+    function sortRoundsData(roundsData, sortBy, reversed) {
+      if (sortBy === 'date') {
+        if (reversed) {
+          return Object.values(roundsData).reverse();
+        } else {
+          return Object.values(roundsData);
+        }
+      } else {
+        return Object.values(roundsData).sort((a, b) => {
           if (sortBy === 'course') {
-            return a['course'] < b['course'];
+            if (reversed) {
+              return b['course'] < a['course'];
+            } else {
+              return a['course'] < b['course'];
+            }
           }
           if (reversed) {
             return b[sortBy] - a[sortBy];
           } else {
             return a[sortBy] - b[sortBy];
           }
-        })
-      })
+        });
+      }
     }
+
+    const roundsData = this.props.roundsData;
+    let lastSortReversed = this.state.lastSortReversed;
+
+    if (sortBy === this.state.lastSortedBy) {
+      this.setState({
+        roundsData: sortRoundsData(roundsData, sortBy, !lastSortReversed)
+      });
+    } else {
+      this.setState({
+        roundsData: sortRoundsData(roundsData, sortBy, reversed)
+      });
+      lastSortReversed = !reversed;
+    }
+
+    this.setState({lastSortedBy: sortBy, lastSortReversed: !lastSortReversed});
+    this.props.clickedSelected();
   }
 
   render() {
     return (
       <div>
         <RoundsHeader
-          onClick={(value, reverse) => this.sortRows(value, reverse)}
+          onClick={(value, reverse) => this.sortRounds(value, reverse)}
         />
         <RoundsList
           roundsData={this.state.roundsData}
