55465748bcef528886abd2dacb04c943f19adeb5
dschmaryl <dschmaryl@gmail.com>
2018-01-03 18:04:49 -0500

    add favicon

--- backend/views/static.py
+++ backend/views/static.py
@@ -6,5 +6,7 @@ from backend import app
 @app.route('/robots.txt')
 @app.route('/humans.txt')
 @app.route('/manifest.json')
+@app.route('/favicon.ico')
+@app.route('/favicon.png')
 def static_file():
     return send_from_directory(app.static_folder, request.path[1:])
