71f99217c28286484b8d36c296c09b53cb32a7ae
dschmaryl <dschmaryl@gmail.com>
2018-02-07 17:00:44 -0500

    add admin views for CourseTee/Hole

--- backend/views/admin.py
+++ backend/views/admin.py
@@ -2,7 +2,7 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 
 from backend import admin, db
-from backend.models import Course, Hole, Round, User
+from backend.models import Course, CourseTee, CourseHole, Hole, Round, User
 
 
 class AdminView(ModelView):
@@ -16,3 +16,5 @@ admin.add_view(AdminView(User, db.session))
 admin.add_view(AdminView(Round, db.session))
 admin.add_view(AdminView(Hole, db.session))
 admin.add_view(AdminView(Course, db.session))
+admin.add_view(AdminView(CourseTee, db.session))
+admin.add_view(AdminView(CourseHole, db.session))
