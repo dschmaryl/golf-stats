771e4f2ff79f7f42aa4afd4989a425fca0cfa200
dschmaryl <dschmaryl@gmail.com>
2019-03-26 17:51:03 -0400

    start some login stuff with react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+
+import { Login } from './components/Login';
 import { Rounds } from './components/Rounds/Rounds';
 import { Stats } from './components/Stats/Stats';
 
@@ -20,31 +22,29 @@ const Header = styled.div`
 `;
 
 export class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { requestFailed: false };
-  }
+  state = { requestFailed: false };
 
-  componentDidMount() {
+  componentDidMount = () =>
     axios
       .get('/api/my_info')
       .then(userData => {
-        if (userData.data['error']) {
+        if (userData.data.error === 'not authorized') {
+          this.setState({ notAuthorized: true });
+        } else if (userData.data['error']) {
           this.setState({ requestFailed: true });
         } else {
           this.setState({ userData: userData.data });
         }
       })
       .catch(() => this.setState({ requestFailed: true }));
-  }
 
-  onSeasonClick(season) {
-    this.setState({ selectedSeason: season });
-  }
+  onSeasonClick = season => this.setState({ selectedSeason: season });
 
   render() {
     if (!this.state.userData) {
-      if (this.state.requestFailed) {
+      if (this.state.notAuthorized) {
+        return <Login />;
+      } else if (this.state.requestFailed) {
         return <p>Failed to retrieve user data</p>;
       } else {
         return <p>Loading user data...</p>;
@@ -55,7 +55,7 @@ export class App extends React.Component {
       <ContainerDiv className="container">
         <Header className="row">
           <div className="col-xs-12">
-            <h3>all statistics:</h3>
+            <h3>all statistics</h3>
           </div>
         </Header>
         <PaddedDiv className="row">
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,40 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const LoginWrapper = styled.div`
-  position: absolute;
-  top: 50%;
-  left 0;
-  width: 100%;
-  height: 400px;
-  margin-top: -260px;
-`;
-
 const LoginContainer = styled.div`
-  max-width: 640px;
-  min-width: 340px;
-  margin: auto;
-  padding: 0;
+  min-width: 320px;
+  max-width: 420px;
+  padding: 10% 0 0 20%;
 `;
 
 const Header = styled.div`
-  font-size: 44px;
+  font-size: 3em;
 `;
 
-export function Login(props) {
-  return (
-    <LoginWrapper>
-      <LoginContainer class="container">
-        <Header class="row">
-          <div class="col-xs-12">welcome</div>
-        </Header>
-        <div class="row">
-          <div class="col-xs-12">name</div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12">password</div>
-        </div>
-      </LoginContainer>
-    </LoginWrapper>
+const InputRow = styled.div`
+  margin: 20px 0 20px 0;
+`;
+
+export class Login extends React.Component {
+  state = { name: '', password: '' };
+
+  render = () => (
+    <LoginContainer>
+      <Header>
+        <div>welcome</div>
+      </Header>
+      <InputRow>
+        <input
+          type="text"
+          className="form-control text-input"
+          value={this.state.name}
+          placeholder="name"
+          onChange={event => this.setState({ name: event.target.value })}
+        />
+      </InputRow>
+      <InputRow>
+        <input
+          type="password"
+          className="form-control text-input"
+          value={this.state.password}
+          placeholder="password"
+          onChange={event => this.setState({ password: event.target.value })}
+        />
+      </InputRow>
+      <InputRow>
+        <input type="submit" value="login" className="btn btn-default" />
+      </InputRow>
+    </LoginContainer>
   );
 }
