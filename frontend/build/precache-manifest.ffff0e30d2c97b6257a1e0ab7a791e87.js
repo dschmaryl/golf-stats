f18fe2d8ebb00c4754685b8acb210f1d802c8d75
dschmaryl <dschmaryl@gmail.com>
2019-03-26 11:35:43 -0400

    update react

---
+++ frontend/build/precache-manifest.ffff0e30d2c97b6257a1e0ab7a791e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc4badc38b459941e2a4",
+    "url": "/static/css/main.c425a997.chunk.css"
+  },
+  {
+    "revision": "cc4badc38b459941e2a4",
+    "url": "/static/js/main.3dbc58e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "889cb48833db009c8087",
+    "url": "/static/js/2.b99732b2.chunk.js"
+  },
+  {
+    "revision": "cae8dbc969fc8b553fe8cb5f2c4b384f",
+    "url": "/index.html"
+  }
+];
