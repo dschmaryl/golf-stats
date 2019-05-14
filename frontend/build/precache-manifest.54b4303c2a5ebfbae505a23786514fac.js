a90fea76f06615dcf8949d811694cd5b01937da8
dschmaryl <dschmaryl@gmail.com>
2019-05-14 17:58:42 -0400

    remove unnecessary stuff

---
+++ frontend/build/precache-manifest.54b4303c2a5ebfbae505a23786514fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fb266d92041abb39e5571813cf3177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e903487c03dc452181",
+    "url": "/static/css/main.adda68d5.chunk.css"
+  },
+  {
+    "revision": "3682c5a6aa93b83ac309",
+    "url": "/static/js/2.6ac25be3.chunk.js"
+  },
+  {
+    "revision": "b7e903487c03dc452181",
+    "url": "/static/js/main.ca30dda7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
