14e4e25e936d061808bcc3ed7d8f233bf121a063
dschmaryl <dschmaryl@gmail.com>
2019-10-29 15:30:02 -0400

    tweak round table formatting

--- frontend/src/screens/Main/Rounds/SelectedRound/SelectedRoundTable.tsx
+++ frontend/src/screens/Main/Rounds/SelectedRound/SelectedRoundTable.tsx
@@ -14,8 +14,24 @@ import { hideRoundDialog } from '../../../../store/rounds/actions';
 import { AppStateType } from '../../../../store/types';
 import { Round } from '../../../../store/rounds/types';
 
+const bigPadding = { padding: '8px' };
+const smallPadding = { padding: '5px' };
+
 const nineHoleArray = (func: Function) =>
   Array.from({ length: 9 }, (v, i) => func(v, i));
+
+const Cell = ({ text }: { text: any }) => (
+  <TableCell
+    align="right"
+    style={('' + text).length > 1 ? smallPadding : bigPadding}
+  >
+    {text}
+  </TableCell>
+);
+
+const CellLeft = ({ text }: { text: any }) => (
+  <TableCell style={smallPadding}>{text}</TableCell>
+);
 
 interface PropTypes {
   selectedRoundIsLoaded: boolean;
@@ -52,29 +68,20 @@ export const SelectedRoundTableComponent: React.FC<PropTypes> = ({
 
     const renderHoles = (startHole: number, stat: string) =>
       nineHoleArray((_: any, i: number) => (
-        <TableCell
-          align="right"
-          style={{ padding: '6px' }}
+        <Cell
+          text={holeStat(i + startHole, stat)}
           key={stat + '_' + (i + startHole)}
-        >
-          {holeStat(i + startHole, stat)}
-        </TableCell>
+        />
       ));
 
     const renderTableRow = (label: string, stat: string) => (
       <TableRow>
-        <TableCell style={{ padding: '8px' }}>{label}:</TableCell>
+        <CellLeft text={label + ':'} />
         {renderHoles(1, stat)}
-        <TableCell align="right" style={{ padding: '8px' }}>
-          {roundData['front_9_' + stat]}
-        </TableCell>
+        <Cell text={roundData['front_9_' + stat]} />
         {renderHoles(10, stat)}
-        <TableCell align="right" style={{ padding: '8px' }}>
-          {roundData['back_9_' + stat]}
-        </TableCell>
-        <TableCell align="right" style={{ padding: '8px' }}>
-          {roundData['total_' + stat]}
-        </TableCell>
+        <Cell text={roundData['back_9_' + stat]} />
+        <Cell text={roundData['total_' + stat]} />
       </TableRow>
     );
 
@@ -82,34 +89,16 @@ export const SelectedRoundTableComponent: React.FC<PropTypes> = ({
       <Table padding="dense">
         <TableHead>
           <TableRow>
-            <TableCell style={{ padding: '8px' }}>hole:</TableCell>
+            <CellLeft text="hole:" />
             {nineHoleArray((_: any, i: number) => (
-              <TableCell
-                align="right"
-                style={{ padding: '6px' }}
-                key={'hole_' + (i + 1)}
-              >
-                {i + 1}
-              </TableCell>
+              <Cell text={i + 1} key={'hole_' + (i + 1)} />
             ))}
-            <TableCell align="right" style={{ padding: '8px' }}>
-              front
-            </TableCell>
+            <Cell text="front" />
             {nineHoleArray((_: any, i: number) => (
-              <TableCell
-                align="right"
-                style={{ padding: '6px' }}
-                key={'hole_' + (i + 10)}
-              >
-                {i + 10}
-              </TableCell>
+              <Cell text={i + 10} key={'hole_' + (i + 10)} />
             ))}
-            <TableCell align="right" style={{ padding: '8px' }}>
-              back
-            </TableCell>
-            <TableCell align="right" style={{ padding: '8px' }}>
-              total
-            </TableCell>
+            <Cell text="back" />
+            <Cell text="total" />
           </TableRow>
         </TableHead>
         <TableBody>
